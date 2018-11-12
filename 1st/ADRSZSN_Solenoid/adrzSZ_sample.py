0e5546b17fa4c4b58198c4e0fcd06a618a4691da
chz100p <chz100p@live.jp>
2018-11-12 09:39:07 +0000

    git ls-files '*.py' | xargs black

--- 1st/ADRSZSN_Solenoid/adrzSZ_sample.py
+++ 1st/ADRSZSN_Solenoid/adrzSZ_sample.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
-#coding: utf-8
+# coding: utf-8
 #####!で始まる1行目の記述はShebangスクリプト自体を実行
 #####2行目に、マジックコメントを記述文字エンコーディング
 #
 #
 # ファイル名：adrszSN_sample.py
 # バージョン：2018/6/20 v1.0  python3用
-#          
+#
 #
 # ビット・トレード・ワン社提供のzerooneシリーズ ソレノイドモジュール(型番：ADRSZSN)用の例題プログラム
-#　著作権者:(C) 2015 ビット・トレード・ワン社
-#　ライセンス: ADL(Assembly Desk License)
+# 　著作権者:(C) 2015 ビット・トレード・ワン社
+# 　ライセンス: ADL(Assembly Desk License)
 #
 #  実行方法： ./adrszSN_sample.py
-#　実行すると　ソレノイドが0.5秒ＯＮになり2秒間隔で繰り返します。
-#  
-#　
+# 　実行すると　ソレノイドが0.5秒ＯＮになり2秒間隔で繰り返します。
+#
+#
 
 
 port = 4
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(port, GPIO.OUT)
 while 1:
--- 3rd/ADRSZLD_OLED_Display/adrszLD-SSD1306SPI-mqtt.py
+++ 3rd/ADRSZLD_OLED_Display/adrszLD-SSD1306SPI-mqtt.py
@@ -5,52 +5,53 @@
 #
 # ファイル名：adrszLD-SSD1306SPI-mqtt.py  python3用
 # バージョン：2018/8/31 v1.0
-#                     
+#
 # ビット・トレード・ワン社提供の ラズパイＺＥＲＯＯＮＥシリーズ
 # ＯＬＥＤ漢字表示基板(型番：ADRSZILD)用のツール
-#　著作権者:(C) 2015 ビット・トレード・ワン社
-#　ライセンス: ADL(Assembly Desk License)
-#  実行方法：読込　./adrszLD-SSD1306SPI-mqtt.py 
+# 　著作権者:(C) 2015 ビット・トレード・ワン社
+# 　ライセンス: ADL(Assembly Desk License)
+#  実行方法：読込　./adrszLD-SSD1306SPI-mqtt.py
 # 　仕様
-#　　　入力：localhostのＭＱＴＴブローカから、TOPIC：adrszLDで文字列入力
+# 　　　入力：localhostのＭＱＴＴブローカから、TOPIC：adrszLDで文字列入力
 #           mqttc.connect("localhost", 1883, 60)
 #           mqttc.subscribe("adrszLD",0)
 #
-#　　　出力：ＭＱＴＴで入力した文字列を、自動スクロール４行で表示
-#          
-#           
+# 　　　出力：ＭＱＴＴで入力した文字列を、自動スクロール４行で表示
+#
+#
 #
 
- 
+
 import time
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
- 
+
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
 #
-#sudo python3 setup.py  install
+# sudo python3 setup.py  install
 import paho.mqtt.client as mqtt
 import json
-gyou1 = 'gyou1  '
-gyou2 = 'gyou2  '
-gyou3 = 'gyou3  '
-gyou4 = 'gyou4  '
+
+gyou1 = "gyou1  "
+gyou2 = "gyou2  "
+gyou3 = "gyou3  "
+gyou4 = "gyou4  "
 # OLEDのサイズ設定
 OLED_WIDTH = 128
 OLED_HEIGHT = 64
- 
+
 # OLEDとSPIバスの設定
 RST = 24
 DC = 23
 SPI_PORT = 0
 SPI_DEVICE = 0
- 
+
 # フォントの設定
-DEFAULT_FONT = '/usr/share/fonts/truetype/fonts-japanese-gothic.ttf'
-#DEFAULT_FONT = '/home/pi/font/misakifont/misaki_gothic.ttf'
+DEFAULT_FONT = "/usr/share/fonts/truetype/fonts-japanese-gothic.ttf"
+# DEFAULT_FONT = '/home/pi/font/misakifont/misaki_gothic.ttf'
 
 FONT_SIZE = 14
 
@@ -58,30 +59,27 @@ FONT_SIZE = 14
 # In[27]:
 
 
-
 def on_connect(mqttc, obj, flags, rc):
     print("rc: " + str(rc))
 
 
-
-
 def on_message(mqttc, obj, msg):
-    global gyou1 
+    global gyou1
     global gyou2
     global gyou3
-    global gyou4  
-    
+    global gyou4
+
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-    #print(str(msg.payload))
-    r = (str(msg.payload))
+    # print(str(msg.payload))
+    r = str(msg.payload)
     print(r)
-    #r4 = 'init  '
-    #print(gyou4)
+    # r4 = 'init  '
+    # print(gyou4)
     gyou4 = gyou3
     gyou3 = gyou2
     gyou2 = gyou1
-    r0=r.replace("b'","")
-    r0=r0.replace("'","")
+    r0 = r.replace("b'", "")
+    r0 = r0.replace("'", "")
     print(r0)
     gyou1 = r0
 
@@ -94,26 +92,27 @@ def on_message(mqttc, obj, msg):
     # バッファの表示
     disp.display()
     # Imageオブジェクトの作成
-    image = Image.new('1', (OLED_WIDTH, OLED_HEIGHT) ,0)
+    image = Image.new("1", (OLED_WIDTH, OLED_HEIGHT), 0)
     # drawオブジェクトの取得
     draw = ImageDraw.Draw(image)
 
-# 文字をimageに描く
+    # 文字をimageに描く
 
-    draw.text((0,0),gyou1, font=jpfont, fill=1)
-    draw.text((0,16),gyou2, font=jpfont, fill=1)
-    draw.text((0,32),gyou3, font=jpfont, fill=1)
-    draw.text((0,48),gyou4, font=jpfont, fill=1)
- 
+    draw.text((0, 0), gyou1, font=jpfont, fill=1)
+    draw.text((0, 16), gyou2, font=jpfont, fill=1)
+    draw.text((0, 32), gyou3, font=jpfont, fill=1)
+    draw.text((0, 48), gyou4, font=jpfont, fill=1)
+
     # imageをOLEDバッファーに書き込む
     disp.image(image)
     # バッファーを表示
     disp.display()
-    #lcd_string(gyou1,LCD_LINE_1)    
-    #lcd_string(gyou2,LCD_LINE_2)    
-    #lcd_string(gyou3,LCD_LINE_3)    
-    #lcd_string(gyou4,LCD_LINE_4)    
-    
+    # lcd_string(gyou1,LCD_LINE_1)
+    # lcd_string(gyou2,LCD_LINE_2)
+    # lcd_string(gyou3,LCD_LINE_3)
+    # lcd_string(gyou4,LCD_LINE_4)
+
+
 def on_publish(mqttc, obj, mid):
     print("mid: " + str(mid))
     pass
@@ -127,36 +126,34 @@ def on_log(mqttc, obj, level, string):
     print(string)
 
 
-
 # In[28]:
 
 
 # SPIオブジェクト
 spi = SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000)
- 
+
 # 128×64ドットのOLEDオブジェクト
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=spi)
- 
+
 # OLEDオブジェクトのスタート
 disp.begin()
- 
+
 # OLEDオブジェクトのバッファークリア
 disp.clear()
 # バッファの表示
 disp.display()
- 
+
 # Imageオブジェクトの作成
-image = Image.new('1', (OLED_WIDTH, OLED_HEIGHT) ,0)
- 
+image = Image.new("1", (OLED_WIDTH, OLED_HEIGHT), 0)
+
 # drawオブジェクトの取得
 draw = ImageDraw.Draw(image)
- 
+
 # TryeTypeフォントオブジェクト
-jpfont = ImageFont.truetype(DEFAULT_FONT, FONT_SIZE, encoding='unic')
+jpfont = ImageFont.truetype(DEFAULT_FONT, FONT_SIZE, encoding="unic")
 
 
 # In[29]:
-
 
 
 mqttc = mqtt.Client()
@@ -167,46 +164,44 @@ mqttc.on_subscribe = on_subscribe
 # Uncomment to enable debug messages
 # mqttc.on_log = on_log
 mqttc.connect("localhost", 1883, 60)
-#mqttc.subscribe("Out-ssd1306spi",0)
-mqttc.subscribe("adrszLD",0)
+# mqttc.subscribe("Out-ssd1306spi",0)
+mqttc.subscribe("adrszLD", 0)
 
 mqttc.loop_start()
-
-   
-   
 
 
 # In[30]:
 
 
-
 # 文字をimageに描く
-gyou1 = 'こんにちは'
-gyou2 = 'ビット・トレード・ワン'
-gyou3 = 'ラズパイzerooneシリーズ '
-gyou4 = 'adrszLD 漢字ボード  '
-draw.text((0,0),gyou1, font=jpfont, fill=1)
-draw.text((0,16),gyou2, font=jpfont, fill=1)
-draw.text((0,32),gyou3, font=jpfont, fill=1)
-draw.text((0,48),gyou4, font=jpfont, fill=1)
- 
+gyou1 = "こんにちは"
+gyou2 = "ビット・トレード・ワン"
+gyou3 = "ラズパイzerooneシリーズ "
+gyou4 = "adrszLD 漢字ボード  "
+draw.text((0, 0), gyou1, font=jpfont, fill=1)
+draw.text((0, 16), gyou2, font=jpfont, fill=1)
+draw.text((0, 32), gyou3, font=jpfont, fill=1)
+draw.text((0, 48), gyou4, font=jpfont, fill=1)
+
 # imageをOLEDバッファーに書き込む
 disp.image(image)
 # バッファーを表示
 disp.display()
 
+
 def main():
-  # Main program block
-  #draw.text((0,0),gyou1, font=jpfont, fill=1)
-  while True:
-      #
-      time.sleep(3)
-if __name__ == '__main__':
-  try:
-    main()
-  except KeyboardInterrupt:
-    pass
-  finally:
-    # OLEDオブジェクトのバッファークリア
-    disp.clear() #lcd_byte(0x01, LCD_CMD)
-      
+    # Main program block
+    # draw.text((0,0),gyou1, font=jpfont, fill=1)
+    while True:
+        #
+        time.sleep(3)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # OLEDオブジェクトのバッファークリア
+        disp.clear()  # lcd_byte(0x01, LCD_CMD)
